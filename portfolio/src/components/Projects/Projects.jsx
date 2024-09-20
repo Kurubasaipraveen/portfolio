a65f1d819e8bbe1080912ec813257c1130546da1
Kurubasaipraveen <154404195+Kurubasaipraveen@users.noreply.github.com>
2024-09-20 09:10:56 +0530

    Projects.jsx

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -21,25 +21,21 @@ const Projects = () => {
           <a href='https://jobbybypraveen.ccbp.tech/login' target="_blank" rel="noreferrer">
             <img
               src='https://res.cloudinary.com/dyutmmnia/image/upload/v1721717535/zoenllpwb85sbvzwuyzb.png'
-              alt="Project 1"
+              alt="Project 2"
               className="img-fluid shadow rounded projImg"
             />
             </a>
           </div>
-          {/* <div className="col-12 col-md-6 px-4 py-4">
-            <img
-              src=''
-              alt="Project 1"
-              className="img-fluid shadow rounded projImg"
-            />
-          </div>
           <div className="col-12 col-md-6 px-4 py-4">
+          <a href='saitictiktoe.ccbp.tech' target="_blank" rel="noreferrer">
             <img
-              src=''
-              alt="Project 1"
+              src='https://res.cloudinary.com/dyutmmnia/image/upload/v1726803630/z39eqohntuv2zsptbcqr.png'
+              alt="Project 3"
               className="img-fluid shadow rounded projImg"
             />
-          </div> */}
+            </a>
+          </div>
+          
         </div>
       </div>
     </div>

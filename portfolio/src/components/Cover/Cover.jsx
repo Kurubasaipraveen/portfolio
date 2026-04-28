3cb726b48574a9fb6383050d4b1c100850fc008d
Kuruba Saipraveen <154404195+Kurubasaipraveen@users.noreply.github.com>
2026-04-28 19:34:47 +0530

    Update Cover.jsx

--- portfolio/src/components/Cover/Cover.jsx
+++ portfolio/src/components/Cover/Cover.jsx
@@ -1,52 +1,75 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import "./Cover.css";
-
 
 const Cover = () => {
   return (
     <div className="container py-5 mb-5 my-5" id="home">
       <div className="row align-items-center my-5 py-5">
+        
+        {/* LEFT SIDE */}
         <div className="col-lg-7 py-5 order-2 order-lg-1 px-4">
-          <h4 className="font-weight-bold">Hello, Its me</h4>
-          <h2 className="font-weight-bold">Kuruba <marque behavior="scroll" direction="left" scrollamount="5"> saipraveen</marque></h2>
-          <h4 className="font-weight-bold mb-4 pb-2">
-            I'm a <span><marquee behavior="scroll" direction="left" scrollamount="10">Full Stack Developer</marquee></span>
+          <h4 className="fw-bold">Hello, It's me</h4>
+
+          <h2 className="fw-bold">
+            Kuruba <span className="scroll-text">Saipraveen</span>
+          </h2>
+
+          <h4 className="fw-bold mb-4 pb-2">
+            I'm a <span className="scroll-text-fast">Full Stack Developer</span>
           </h4>
-          <p className="font-weight-normal">
-            I'm a Student. Insterested in Full Stack developer
+
+          <p>
+            I'm a student, interested in Full Stack Development.
           </p>
 
+          {/* SOCIAL LINKS */}
           <div className="social-media py-3">
-            <Link
-              to="https://www.linkedin.com/in/kurubasaipraveen/"
+            <a
+              href="https://www.linkedin.com/in/kurubasaipraveen/"
               target="_blank"
+              rel="noopener noreferrer"
               className="mx-2"
             >
-              <i class="bi bi-linkedin"></i>
-            </Link>
-            <Link
-              to="https://github.com/Kurubasaipraveen/"
+              <i className="bi bi-linkedin"></i>
+            </a>
+
+            <a
+              href="https://github.com/Kurubasaipraveen/"
               target="_blank"
+              rel="noopener noreferrer"
               className="mx-2"
             >
-              <i class="bi bi-github"></i>
-            </Link>
-            <Link
-              to="mailto:ksaipraveen6@gmail.com?subject=Hello&body=Hi%20there,%0A%0AJust%20wanted%20to%20say%20hello!"
-              target="_blank"
+              <i className="bi bi-github"></i>
+            </a>
+
+            <a
+              href="mailto:ksaipraveen6@gmail.com"
               className="mx-2"
             >
-              <i class="bi bi-envelope"></i>
-            </Link>
+              <i className="bi bi-envelope"></i>
+            </a>
           </div>
-          <a href="https://drive.google.com/uc?export=download&id=1uGjOCuBjDIvRxk3N87gOkvItfEWcxmEO" download="resume.pdf">
-            <button className="button my-3">Download Resume🫠</button>
+
+          {/* RESUME */}
+          <a
+            href="https://drive.google.com/uc?export=download&id=1uGjOCuBjDIvRxk3N87gOkvItfEWcxmEO"
+            download="resume.pdf"
+          >
+            <button className="button my-3">
+              Download Resume
+            </button>
           </a>
         </div>
+
+        {/* RIGHT SIDE */}
         <div className="col-12 col-lg-4 offset-lg-1 text-center order-1 order-lg-2 py-5">
-          <img src="https://res.cloudinary.com/dyutmmnia/image/upload/v1777345051/file_000000002f5872088c0cad8d3621d5fb_ryo8qy.png" alt="sisi_tarak" className="img-fluid" />
+          <img
+            src="https://res.cloudinary.com/dyutmmnia/image/upload/v1777345051/file_000000002f5872088c0cad8d3621d5fb_ryo8qy.png"
+            alt="Kuruba Saipraveen"
+            className="img-fluid"
+          />
         </div>
+
       </div>
     </div>
   );
